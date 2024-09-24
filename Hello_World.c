eaeba7224e346d32d2c93ad3ba51b3c21cef9fc3
Eugo21 <eugenecodes3@gmail.com>
2024-09-25 00:11:02 +0300

    Hello world

---
+++ Hello_World.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    char name[] = "Eugene", hobby[] = "Gaming";
+    int age = 20;
+
+    printf("Hi, my name is %s, am %dyrs old and my fav hobby is %s.", name, age, hobby);
+    return 0; 
+}
