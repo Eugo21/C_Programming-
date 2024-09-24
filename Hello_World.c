8f282344547994ddb36d00d76ed7b700efd5481e
Eugo21 <eugenecodes3@gmail.com>
2024-09-25 00:21:01 +0300

    Update

--- Hello_World.c
+++ Hello_World.c
@@ -5,6 +5,6 @@ int main()
     char name[] = "Eugene", hobby[] = "Gaming";
     int age = 20;
 
-    printf("Hi, my name is %s, am %dyrs old and my fav hobby is %s.", name, age, hobby);
+    printf("Hi, my name is %s, am %dyrs old and my fav hobby is %s.", name, age, hobby);  // prints my name age and my favorite hobby.
     return 0; 
 }
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# C_Programming-
+# This Repo will contine all my C projects
+
+# Day 1
+
+ 1. C program to print hello world.
