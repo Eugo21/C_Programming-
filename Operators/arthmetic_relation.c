a887d1599d50f8403504e4f138de878273ec8169
Eugo21 <eugenecodes3@gmail.com>
2024-10-10 18:03:56 +0300

    Opertors

---
+++ Operators/arthmetic_relation.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int x = 7, y = 5;
+    
+    
+    
+    if (x > y)
+    {
+        printf("x is greater than y\n");
+    }
+    else if (x < y)
+    {
+        printf("x is less than y\n");
+    }
+}
