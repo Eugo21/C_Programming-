e14f50b0563e001cade29bbe2ac607f8ed74fb6b
Eugo21 <eugenecodes3@gmail.com>
2024-11-07 20:48:57 +0300

    job charged

--- Book_HomeWorks/job_charged.c
+++ Book_HomeWorks/job_charged.c
@@ -5,8 +5,11 @@ int main() {
     double hours, parts, charge;
 
     // Combined prompts into one line
-    printf("Type how many hours you worked and the cost of parts (separated by a space): ");
-    scanf("%lf %lf", &hours, &parts);
+    printf("How my hours did you work?: ");
+    scanf("%lf", &hours);
+
+    printf("Costs of the parts?: ");
+    scanf("%lf", &parts);
     
     // Calculate the charge
     charge = hours * 100 + parts;
@@ -15,7 +18,7 @@ int main() {
     if (charge < 150) {
         printf("Your charge is less than 150: %.2lf\n", charge);
     } else {
-        printf("Your total charge is: %.2lf\n", charge);
+        printf("Your total charge is: $%.2lf\n", charge);
     }
     
     return 0;
