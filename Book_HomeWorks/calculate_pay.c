56b868c6e1487eaccfdc11c5269531b8e2de2acb
Eugo21 <eugenecodes3@gmail.com>
2024-11-09 20:28:01 +0300

    pay_calculater

--- Book_HomeWorks/calculate_pay.c
+++ Book_HomeWorks/calculate_pay.c
@@ -1,27 +1,39 @@
 #include <stdio.h>
 
-
-
 int main()
 {
-    int pay, hours, rate;
+    //* Declare variables to store hours worked, hourly rate, regular pay, overtime pay, and gross pay
+    double hours, rate, regPay, ovtPay, grossPay;
 
+    //! Prompt user to input the number of hours worked and store it in 'hours'
     printf("How many hours did you work: ");
-    scanf("%d", &hours);
+    scanf("%lf", &hours);
 
-    printf("What is your hourly rate: ");
-    scanf("%d", &rate);
+    //*! Prompt user to input the hourly rate of pay and store it in 'rate'
+    printf("What is your hourly rate of pay: ");
+    scanf("%lf", &rate);
 
+    //* Calculate regular pay and overtime pay based on hours worked
     if (hours <= 40)
     {
-        pay = hours * rate;
-        printf("Your pay is: %d\n", pay);
+        //! If hours worked are 40 or less, all hours are regular hours
+        regPay = hours * rate;
+        ovtPay = 0;
     }
     else
     {
-        pay = (40 * rate) + ((hours - 40) * (rate * 1.5));
-        printf("Your pay is: %d\n", pay);
+        //! If hours exceed 40, calculate regular pay for 40 hours and overtime for remaining hours
+        regPay = 40 * rate;
+        ovtPay = ((hours - 40) * rate * 1.5);  // Overtime pay is 1.5 times the regular rate
     }
     
-    
+    //* Calculate gross pay by adding regular and overtime pay
+    grossPay = regPay + ovtPay;
+
+    //! Display the results
+    printf("Regular pay: $%.2lf\n", regPay);
+    printf("Overtime pay: $%.2lf\n", ovtPay);
+    printf("Gross pay: $%.2lf\n", grossPay);
+
+    return 0;
 }
